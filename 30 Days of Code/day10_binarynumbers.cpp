de37bb9ab64c307e3786a30602315a02f4b49ba2
John Vogel <22157170+vogel005@users.noreply.github.com>
2019-06-18 14:53:08 -0700

    30days of code +2
    added day 9 and 10
    recursion and binary numbers.

---
+++ 30 Days of Code/day10_binarynumbers.cpp
@@ -0,0 +1,52 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+
+int main()
+{
+    int n;
+    cin >> n;
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+
+  /*
+  PURPOSE: Converts decimal number into binary, then counts what the longest consecutive 1 count is.
+  DATE: 6-18-2019
+  INPUT(via hackerrank):
+  * int n; a number to be converted into binary.
+  VARIABLES:
+  * int r; remainder after dividing n by 2, used to find the binary number
+  * string b; a binary string. 
+  * int c; counts consecutive 1's in the binary string.
+  * int max; the maximum count found.
+  */
+
+  int r;
+  string b; 
+  int c=0; 
+  int max=0;
+
+  while(n>0) //binary conversion
+  {
+    r=n%2;
+    n/=2;
+    b.push_back('0'+r);
+  }
+  reverse(b.begin(), b.end());
+
+  for(int i=0;i<b.length();i++) //consecutive 1 counting
+  {
+    if(b[i]=='1')
+      c++;
+    else
+      c=0;
+    
+    if(c>max)
+      max=c;
+  }
+  
+  cout<<max<<endl;
+  return 0;
+}
---
+++ 30 Days of Code/day9_recursion.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+//returns factorial of n
+//DATE: 6-17-2019
+int factorial(int n) {
+  return (n==1) ? n : n*factorial(n-1);
+}
+
+int main() //hackerrank code
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    int n;
+    cin >> n;
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+    int result = factorial(n);
+
+    fout << result << "\n";
+
+    fout.close();
+
+    return 0;
+}
